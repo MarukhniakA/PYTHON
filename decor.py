854b77cee168b8bad85c2823c30d7f480ef37cc5
unknown <you@example.comvfhepf@mail.ru>
2023-04-12 18:36:31 +0300

    decorator

---
+++ decor.py
@@ -0,0 +1,14 @@
+login = str(input("Введите логин: "))
+
+def decorator(func):
+    def wrapper(*args, **kwargs):
+      if login == "admin":
+       func(*args, **kwargs)
+      if login != "admin":
+       return print("Доступ запрещен!")
+    return wrapper
+
+@decorator
+def my_func(check):
+    print("Сумма на вашем счете: {} рублей".format(check))
+my_func(100)
